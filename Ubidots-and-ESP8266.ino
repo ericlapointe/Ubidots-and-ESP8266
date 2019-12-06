1404aeb0cea268111a26d48c9c4441940064f09f
Eric Lapointe <32047584+ericlapointe@users.noreply.github.com>
2019-12-06 18:59:50 -0500

    Use new const

--- Ubidots-and-ESP8266.ino
+++ Ubidots-and-ESP8266.ino
@@ -96,9 +96,9 @@ void loop(void) {
   float value1 = random(0, 9) * 10;
   float value2 = random(0, 9) * 100;
   float value3 = random(0, 9) * 1000;
-  ubidots.add("Variable_Name_One", value1);  // Change for your variable name
-  ubidots.add("Variable_Name_Two", value2);
-  ubidots.add("Variable_Name_Three", value3);
+  ubidots.add(UBIDOTS_VAR1, value1);  // Change for your variable name
+  ubidots.add(UBIDOTS_VAR2, value2);
+  ubidots.add(UBIDOTS_VAR3, value3);
 
   bool bufferSent = false;
   bufferSent = ubidots.send();  // Will send data to a device label that matches the device Id
